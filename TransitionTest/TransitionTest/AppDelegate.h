06ff514191cdedfeb98b42075ff7d686d4113a47
Valentina <chrnoeva@magora.systems>
2016-03-25 14:25:42 +0600

    create project

---
+++ TransitionTest/TransitionTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TransitionTest
+//
+//  Created by Valentina Chernoeva on 25.03.16.
+//  Copyright © 2016 Valentina Chernoeva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TransitionTest/TransitionTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TransitionTest
+//
+//  Created by Valentina Chernoeva on 25.03.16.
+//  Copyright © 2016 Valentina Chernoeva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
